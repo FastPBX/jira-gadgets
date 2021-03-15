0362a8a6410e8e888f7d7b22117d24201fe5c253
David Melo <davidmelo@desmdesigns.com>
2021-03-15 17:54:58 -0400

    Update quick-search.js

--- quick-search/quick-search.js
+++ quick-search/quick-search.js
@@ -1,1 +1,6 @@
+gadgets.window.adjustHeight();
 
+function doNumbersSearch () {
+  var number = "800";
+  window.location.href = "https://tickets-internal.fpool.org/issues/?jql=%22Number(s)%20Affected%20Preview%22%20~%20%22*"+number+"*%22";
+}
